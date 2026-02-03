c728f90af158e4d23e9268ee4897f80eb02218af
tathagatvetale317 <tathagatvetale317@eng.rizvi.edu.in>
2026-02-03 21:10:31 +0530

    Experiment_4.py

---
+++ Experiment_4.py
@@ -0,0 +1,5 @@
+p = float(input("Enter principal amount: "))
+r = float(input("Enter rate of interest: "))
+t = float(input("Enter time(in years): "))
+si = (p * r * t)/100
+print("Simple interest=", si)
