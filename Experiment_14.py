5bc181b637de325cebd8828bae4b1ddd12fa75a5
tathagatvetale317 <tathagatvetale317@eng.rizvi.edu.in>
2026-03-04 21:57:54 +0530

    Update Experiment_14.py

--- Experiment_14.py
+++ Experiment_14.py
@@ -1,10 +1,8 @@
 try:
-    num1 = int(input("enter first value: "))
-    num2 = int(input("enter second value: "))
-    division = num1 / num2
+    a = float(input("Enter first number: "))
+    b = float(input("Enter second number: "))
+    print("Result:", a / b)
 except ZeroDivisionError:
-    print("number cannot divide by zero")
+    print("Cannot divide by zero!")
 except ValueError:
-    print("invalid input")
-else:
-    print("ans:", division)
+    print("Please enter numbers only!")
